8ca59ff2ab6ede0d47c20efb50b11f0e8a35d360
Felix Jung <felix@jung.fm>
2018-05-31 12:18:45 +0300

    Fix linting error

--- babel.js
+++ babel.js
@@ -2,4 +2,5 @@ const { resolve } = require('path');
 
 const configPath = resolve(__dirname, './dist');
 
+// eslint-disable-next-line import/no-dynamic-require
 module.exports = require(configPath).configs.babel;
