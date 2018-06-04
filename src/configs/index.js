d1b2a91fae5fe71258ec9807211707a5ebb698b3
Felix Jung <felix@jung.fm>
2018-06-04 22:56:06 +0200

    Fix eslint import

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,4 +1,4 @@
-import * as eslint from './eslintrc';
+import * as eslint from './eslint';
 import * as babel from './babel';
 import * as prettier from './prettier';
 import * as plop from './plop';
