651a32ecab0ffb9e7dfae0695ad2f293fb3f2053
Felix Jung <felix@jung.fm>
2018-11-22 19:58:06 +0200

    Add coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   clearMocks: true,
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: '__coverage__',
+  coveragePathIgnorePatterns: ['/node_modules/', 'plop/templates'],
   coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  collectCoverageFrom: ['src/**/*.js'],
   testEnvironment: 'node',
   setupTestFrameworkScriptFile: './testSetup.js'
 };
