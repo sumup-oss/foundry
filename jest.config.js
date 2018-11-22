0472770e3c2f7a958dc773f6c6654999e8a8a14b
Felix Jung <felix@jung.fm>
2018-11-23 00:58:32 +0200

    Add consistant coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   clearMocks: true,
   rootDir: 'src',
-  coverageDirectory: '../__coverage__',
-  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  coverageDirectory: '../__reports__/jest',
+  coverageReporters: ['default', 'junit'],
   coveragePathIgnorePatterns: ['node_modules'],
   testEnvironment: 'node',
   setupTestFrameworkScriptFile: '../testSetup.js'
