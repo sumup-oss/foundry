54b29c5004bb78c1ac419d9526e7c969c80850af
Connor Bär <connor.baer@sumup.com>
2019-01-25 23:07:54 +0100

    style: Ignore eslint warnings

--- src/configs/husky.js
+++ src/configs/husky.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
 export const base = {
   hooks: {
     'pre-commit': 'foundry run lint-staged'
--- src/configs/lint-staged.js
+++ src/configs/lint-staged.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
 export const base = {
   linters: {
     '*.js': ['foundry run eslint --fix', 'git add']
