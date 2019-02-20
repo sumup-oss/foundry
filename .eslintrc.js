e5acbe9b8c37e29109ec5f443ff6c35df56ab75d
Felix Jung <felix.jung@sumup.com>
2019-02-20 17:25:40 +0100

    Add copyright headers

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,1 @@
-const { node, test, copyright } = require('@sumup/foundry/eslint');
-const { merge } = require('@sumup/foundry');
-
-module.exports = merge(node, test, copyright);
+module.exports = require('@sumup/foundry/eslint').node;
--- src/configs/eslint/index.js
+++ src/configs/eslint/index.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2019, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import merge from 'webpack-merge';
 
 export const overwritePresets = merge({
@@ -12,7 +27,7 @@ export const base = {
     'plugin:jest/recommended',
     'plugin:prettier/recommended'
   ],
-  plugins: ['prettier', 'jest', 'cypress'],
+  plugins: ['prettier', 'jest', 'cypress', 'notice'],
   rules: {
     'no-use-before-define': ['error', { functions: false }],
     'max-len': [
@@ -28,6 +43,14 @@ export const base = {
     'no-underscore-dangle': [
       'error',
       { allow: ['__DEV__', '__PRODUCTION__', '__TEST__'] }
+    ],
+    'notice/notice': [
+      'error',
+      {
+        templateFile: `${__dirname}/copyright.tpl`,
+        templateVars: { NAME: 'SumUp Ltd.' },
+        varRegexps: { NAME: /SumUp Ltd\./ }
+      }
     ]
   },
   globals: {
@@ -120,19 +143,5 @@ export const react = overwritePresets(base, {
     browser: true
   }
 });
-
-export const copyright = {
-    plugins:['notice'],
-    rules: {
-      'notice/notice': [
-        'error',
-        {
-          templateFile: `${__dirname}/copyright.tpl`,
-          templateVars:{ NAME: 'SumUp Ltd.' },
-          varRegexps:{ NAME: /SumUp Ltd./ }
-        }
-      ]
-    }
-}
 
 export const ESLINT_CONFIGS = ['base', 'node', 'react'];
