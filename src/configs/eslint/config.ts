9f637a8a59927098d96ee2329d631ff00ce4b787
nico <nicosommi@gmail.com>
2020-06-02 14:57:28 +0200

    feat(eslint): add import/order rule (#109)
    * Adds organise import lint rule
    
    * Refresh snapshot after test with node 10
    
    BREAKING CHANGE: Added the import/order Eslint rule. Run yarn lint:fix to automatically fix all new errors.

--- src/configs/eslint/config.ts
+++ src/configs/eslint/config.ts
@@ -14,6 +14,7 @@
  */
 
 import process from 'process';
+
 import { flow, mergeWith, isArray, isObject, isEmpty, uniq } from 'lodash/fp';
 
 import { Options, Language, Environment, Framework } from '../../types/shared';
@@ -65,6 +66,7 @@ const base = {
       { allow: ['__DEV__', '__PRODUCTION__', '__TEST__'] },
     ],
     'import/prefer-default-export': 'off',
+    'import/order': ['error', { 'newlines-between': 'always' }],
     // The rules below are already covered by prettier.
     'quote-props': 'off',
     'comma-dangle': 'off',
