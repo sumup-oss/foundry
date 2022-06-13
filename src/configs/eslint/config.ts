c004e6c87b70e0644553432669e2ef511b450624
Connor Bär <connor-baer@users.noreply.github.com>
2022-06-13 21:02:00 +0200

    feat(eslint): improve ESLint rules for API routes (#565)
    * Disable compat rules for API routes
    
    * Disable no-console rule for API routes

--- src/configs/eslint/config.ts
+++ src/configs/eslint/config.ts
@@ -45,6 +45,18 @@ function customizer(
   return undefined;
 }
 
+const UNIT_TEST_FILES = [
+  '**/*.spec.*',
+  '**/jest*',
+  '**/setupTests.*',
+  '**/test-utils.*',
+  '**/*Fixtures.*',
+  '**/__fixtures__/**/*',
+  '**/__mocks__/**/*',
+];
+
+const NODE_FILES = ['api/**/*', 'pages/api/**/*', 'src/pages/api/**/*'];
+
 const sharedRules = {
   'curly': ['error', 'all'],
   'no-use-before-define': 'off',
@@ -125,18 +137,14 @@ const base = {
         'notice/notice': 'off',
       },
     },
+    {
+      files: NODE_FILES,
+      rules: {
+        'no-console': 'off',
+      },
+    },
   ],
 };
-
-const unitTestFiles = [
-  '**/*.spec.*',
-  '**/jest*',
-  '**/setupTests.*',
-  '**/test-utils.*',
-  '**/*Fixtures.*',
-  '**/__fixtures__/**/*',
-  '**/__mocks__/**/*',
-];
 
 function customizeLanguage(language?: Language) {
   const languageMap = {
@@ -236,7 +244,7 @@ function customizeEnvironments(environments?: Environment[]) {
       },
       overrides: [
         {
-          files: unitTestFiles,
+          files: [...UNIT_TEST_FILES, ...NODE_FILES],
           rules: {
             'compat/compat': 'off',
           },
@@ -370,7 +378,7 @@ function customizeFramework(frameworks?: Framework[]) {
     [Framework.JEST]: {
       overrides: [
         {
-          files: unitTestFiles,
+          files: UNIT_TEST_FILES,
           extends: ['plugin:jest/recommended'],
           plugins: ['jest'],
           env: { 'jest/globals': true },
@@ -406,7 +414,7 @@ function customizeFramework(frameworks?: Framework[]) {
     [Framework.TESTING_LIBRARY]: {
       overrides: [
         {
-          files: unitTestFiles,
+          files: UNIT_TEST_FILES,
           extends: ['plugin:testing-library/react'],
           plugins: ['testing-library'],
         },
