78c24e26eab5dcc9ace8528eaf90e6e5ee6d7490
Johannes Waigel <waigel@klexhub.com>
2023-03-23 18:59:20 +0100

    Update GitHub action by removing deprecated functions

--- src/configs/ci/github-actions.ts
+++ src/configs/ci/github-actions.ts
@@ -34,7 +34,7 @@ jobs:
           node-version: 16.x
       - name: Get yarn cache directory path
         id: yarn-cache-dir-path
-        run: echo "::set-output name=dir::$(yarn cache dir)"
+        run: echo "dir=$(yarn cache dir)" >> $GITHUB_OUTPUT
       - name: Cache dependencies
         uses: actions/cache@v2
         id: yarn-cache
@@ -47,7 +47,7 @@ jobs:
         run: yarn --pure-lockfile --prefer-offline
 {{#includes presets "lint"}}
       - name: Lint
-        run: yarn lint:ci
+        run: yarn lint
 {{/includes}}
 {{#includes presets "release"}}
       - name: Release
